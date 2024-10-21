9c8564e7d428c6a6cb38869ab94c06cff5d69d5c
ayano0o <acarver0514@gmail.com>
2024-10-21 14:19:34 -0500

    added tic-tac-toe

---
+++ human_player.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#include "human_player.hpp"
+
+HumanPlayer::HumanPlayer(Board *board, char mark)
+{
+  this->board = board;
+  this->mark = mark;
+}
+
+void HumanPlayer::get_move()
+{
+int move;
+    bool validMove = false;
+
+    while (!validMove) {
+        std::cout << "What is your move? ";
+        std::cin >> move;
+
+        if (move < 1 || move > 9) {
+            std::cout << "Invalid move! Please enter a number between 1 and 9." << std::endl;
+            continue;
+        }
+
+        if (this->board->*********(move)) {
+            this->board->move(move, this->mark);
+            validMove = true;
+        } else {
+            std::cout << "That cell is already taken! Please choose another." << std::endl;
+        }
+    }
+}
+
